ecf70080a3988aa6c20f5b41a7bd430a15f63d89
Namwon Kim <knw1234@gmail.com>
2022-04-07 18:31:56 +0900

    64비트 지원

--- random_code_ui/Form1.Designer.cs
+++ random_code_ui/Form1.Designer.cs
@@ -28,13 +28,169 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAllowedText = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDigit = new System.Windows.Forms.NumericUpDown();
+            this.txtTotal = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnFile = new System.Windows.Forms.Button();
+            this.lbSaveFile = new System.Windows.Forms.Label();
+            this.btnGen = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.txtDigit)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtTotal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(28, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "허용할 문자";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtAllowedText
+            // 
+            this.txtAllowedText.Location = new System.Drawing.Point(103, 17);
+            this.txtAllowedText.Name = "txtAllowedText";
+            this.txtAllowedText.Size = new System.Drawing.Size(393, 21);
+            this.txtAllowedText.TabIndex = 1;
+            this.txtAllowedText.Text = "CEFHKMNPTWXY3479";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(28, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "코드 자릿수";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtDigit
+            // 
+            this.txtDigit.Location = new System.Drawing.Point(103, 44);
+            this.txtDigit.Name = "txtDigit";
+            this.txtDigit.Size = new System.Drawing.Size(393, 21);
+            this.txtDigit.TabIndex = 3;
+            this.txtDigit.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtDigit.Value = new decimal(new int[] {
+            8,
+            0,
+            0,
+            0});
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(103, 71);
+            this.txtTotal.Maximum = new decimal(new int[] {
+            999999999,
+            0,
+            0,
+            0});
+            this.txtTotal.Minimum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.Size = new System.Drawing.Size(393, 21);
+            this.txtTotal.TabIndex = 5;
+            this.txtTotal.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtTotal.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(28, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "생성갯수";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(28, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 12);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "저장위치";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // btnFile
+            // 
+            this.btnFile.Location = new System.Drawing.Point(103, 97);
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(393, 23);
+            this.btnFile.TabIndex = 7;
+            this.btnFile.Text = "저장위치선택";
+            this.btnFile.UseVisualStyleBackColor = true;
+            this.btnFile.Click += new System.EventHandler(this.btnFile_Click);
+            // 
+            // lbSaveFile
+            // 
+            this.lbSaveFile.AutoSize = true;
+            this.lbSaveFile.Location = new System.Drawing.Point(101, 126);
+            this.lbSaveFile.Name = "lbSaveFile";
+            this.lbSaveFile.Size = new System.Drawing.Size(0, 12);
+            this.lbSaveFile.TabIndex = 8;
+            this.lbSaveFile.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnGen
+            // 
+            this.btnGen.Location = new System.Drawing.Point(103, 154);
+            this.btnGen.Name = "btnGen";
+            this.btnGen.Size = new System.Drawing.Size(393, 23);
+            this.btnGen.TabIndex = 9;
+            this.btnGen.Text = "생성";
+            this.btnGen.UseVisualStyleBackColor = true;
+            this.btnGen.Click += new System.EventHandler(this.btnGen_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(521, 189);
+            this.Controls.Add(this.btnGen);
+            this.Controls.Add(this.lbSaveFile);
+            this.Controls.Add(this.btnFile);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDigit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAllowedText);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "코드 생성기";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.txtDigit)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.txtTotal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAllowedText;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown txtDigit;
+        private System.Windows.Forms.NumericUpDown txtTotal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnFile;
+        private System.Windows.Forms.Label lbSaveFile;
+        private System.Windows.Forms.Button btnGen;
     }
 }
 
--- random_code_ui/Form1.cs
+++ random_code_ui/Form1.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
+using System.IO;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace random_code_ui
@@ -16,5 +13,97 @@ namespace random_code_ui
         {
             InitializeComponent();
         }
+
+        private void btnFile_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog();
+            save.Filter = "TXT File|*.txt";
+            save.Title = "Save File";
+            save.ShowDialog();
+
+            if (save.FileName != "")
+            {
+                lbSaveFile.Text = save.FileName;
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbSaveFile.Text = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\gen.txt";
+        }
+
+
+        private void btnGen_Click(object sender, EventArgs e)
+        {
+            string msg =
+@"동작 중엔 멈출 수 없습니다. 
+생성 갯수가 크면 중복체크 때문에 오래걸립니다. (5천만개 생성에 40초 내외)
+PC의 메모리가 부족하면 오류가 나면서 멈출 수 있습니다. (메모리 16기가 이상 권장)
+그래도 생성하시겠습니까?";
+
+            var result = MessageBox.Show(msg, "생성하기", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                btnGen.Enabled = false;
+
+                Thread trd = new Thread(new ThreadStart(this.Generate));
+                trd.IsBackground = true;
+                trd.Start();
+
+
+            }
+        }
+
+
+        private static Dictionary<string, object> map = new Dictionary<string, object>();
+
+        private void Generate()
+        {
+            map.Clear();
+            DoRandomDic();
+
+            using (var fs = new FileStream(this.lbSaveFile.Text, FileMode.Create, FileAccess.Write, FileShare.None, 1024 * 1024, FileOptions.Asynchronous | FileOptions.SequentialScan))
+            {
+                using (StreamWriter sw = new StreamWriter(fs, new UTF8Encoding(false)))
+                {
+                    foreach (var k in map.Keys)
+                    {
+                        sw.WriteLine(k);
+                    }
+                }
+            }
+
+            this.Invoke(new MethodInvoker(delegate
+            {
+                MessageBox.Show("완료!");
+                btnGen.Enabled = true;
+            }));
+
+        }
+
+
+        public void DoRandomDic()
+        {
+
+            Random r = new Random(Guid.NewGuid().GetHashCode());
+
+            string AllowedChars = this.txtAllowedText.Text;
+            int CodeCipher = (int)this.txtDigit.Value;
+            int WantedCount = (int)this.txtTotal.Value;
+
+            while (map.Count < WantedCount)
+            {
+                StringBuilder sb = new StringBuilder();
+                for (var i = 0; i < CodeCipher; i++)
+                {
+                    sb.Append(AllowedChars[r.Next(AllowedChars.Length)]);
+                }
+                string code = sb.ToString();
+                if (!map.ContainsKey(code))
+                    map.Add(sb.ToString(), null);
+            }
+        }
+
     }
 }
